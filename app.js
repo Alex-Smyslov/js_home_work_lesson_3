f68ae54e0cf29b00b0a69f23bc9911b6f42e1cf8
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2022-11-05 11:58:16 +0300

    Commit 2

--- app.js
+++ app.js
@@ -1,1 +1,77 @@
-console.log("Hello World!");
+let age = "dfcff";
+age = +age;
+if (!isNaN(age)) {
+	if (age === null) {
+		console.log("Некорректный возраст!");
+	}
+}	else {
+	console.log("Некорректный возраст!");
+}
+
+
+
+if(age) {
+		if (age > 18) {
+		console.log("Добро пожаловать!");
+	}	else if (age === 18) {
+		console.log("Поздравляем с совершеннолетием!");
+	}	else {
+		console.log("Вы ещё слишком малы!");
+	}
+} else {
+	console.log("Укажите Ваш возраст!");
+}
+
+let gender = "Чен";
+if (gender.toLocaleLowerCase() === "муж") {
+	console.log("Вам налево");
+}	else if (gender.toLocaleLowerCase() === "жен") {
+	console.log("Вам направо");
+}	else {
+	console.log("Вам в Европу!!!!!!");
+}
+
+
+
+
+// let isUser = false;
+// let typeUser = isUser ? "Пользователь" : "Бот" ;
+// console.log("Добро пожаловать" + typeUser);
+
+let typeUser = "admin"; //user, guest, bot
+switch (typeUser) {
+	case "admin":
+		console.log("Вы admin!");
+	break;
+	case "user":
+		console.log("Вы user!");
+	break;
+	case "guest":
+		console.log("Вы guest!");
+	break;
+	case "bot":
+		console.log("Вы bot!");
+	break;
+	default:
+		console.log("Мы вас не знаем!");
+	break;
+}
+let i = 0;
+const lastNumber = 20;
+let resultStrig = '';
+while (++i<=lastNumber) {
+	resultStrig += `${i}${i !== lastNumber ? ', ' : ''}`;
+}
+console.log(resultStrig);
+
+let j = 20;
+let resultStrig2 = '';
+while (j>=0) {
+	resultStrig2 += `${j}${j !== 0 ? ', ' : ''}`;
+	j--;
+}
+console.log(resultStrig2);
+
+// for (let k=1; k<=10; k++) {
+// 	console.log(k);
+// }
